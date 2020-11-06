218a06c84a943c8a4eb978f195f5bbdd4ed458b4
radlee <prof.lolito@gmail.com>
2020-11-07 01:06:36 +0200

    Initial Commit

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, Button, 
+  Platform, View,
+  SafeAreaView,
+  Dimensions
+} from 'react-native';
 
 export default function App() {
+  console.log(Dimensions.get('screen'))
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: "30%"
+        }}
+      >
+        <Text>Some Text</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +30,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "web" ? StatusBar.currentHeight : 0
   },
 });
